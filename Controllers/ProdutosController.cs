eb41b62cbe064d1b60f02ad975eb1dde7eda7071
Fabricio Carvalho <fabriciosipauba2@gmail.com>
2024-09-26 13:48:58 -0300

    Att do método de registro e busca dos produtos

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -3,11 +3,11 @@ using InventoryManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace InventoryManager.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
+    
     public class ProdutosController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -23,25 +23,56 @@ namespace InventoryManager.Controllers
             return _context.Produtos.ToList();
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<Produto> GetProduto(int id)
+        [HttpGet("busca")]
+        public ActionResult<IEnumerable<Produto>> GetProdutosByFiltro([FromQuery] string? categoria = null,
+        string? tipo = null, [FromQuery] string? marca = null)
         {
-            var produto = _context.Produtos.Find(id);
-            if (produto == null)
+
+            var produtos = _context.Produtos.AsQueryable();
+
+            if (!string.IsNullOrEmpty(categoria))
             {
-                return NotFound();
+                produtos = produtos.Where(p => p.Categoria == categoria);
             }
-            return produto;
+
+            if (!string.IsNullOrEmpty(tipo))
+            {
+                produtos = produtos.Where(p => p.Tipo == tipo);
+            }
+
+            if (!string.IsNullOrEmpty(marca))
+            {
+                produtos = produtos.Where(p => p.Marca == marca);
+            }
+
+            if (!produtos.Any())
+            {
+                return NotFound("Nenhum ítem encontrado com os dados fornecidos.");
+            }
+
+            return Ok(produtos.ToList());
         }
 
         [HttpPost]
-        public ActionResult<Produto> PostProduto(Produto produto)
+        public async Task<ActionResult<Produto>> PostProdutoAsync(Produto produto)
         {
+
+            var produtoExistente = await _context.Produtos
+        .FirstOrDefaultAsync(p => p.Modelo == produto.Modelo);
+
+            if (produtoExistente != null)
+            {
+                return Conflict(new
+                {
+                    erroCadastro = "Esse ítem já está cadastrado",
                     produto = produtoExistente
+                });
+            }
+
             _context.Produtos.Add(produto);
             _context.SaveChanges();
 
-            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+            return CreatedAtAction(nameof(GetProdutos), new { id = produto.Id }, produto);
         }
 
         [HttpPut("{id}")]
@@ -63,14 +94,20 @@ namespace InventoryManager.Controllers
         {
             var produto = _context.Produtos.Find(id);
             if (produto == null)
-            {
-                return NotFound();
-            }
+                return Conflict (new
+                {
+                    mensagem = $"ID {id} não corresponde a nenhum ítem no registro",
+                    produto
+                });
 
             _context.Produtos.Remove(produto);
             _context.SaveChanges();
 
-            return NoContent();
+            return Ok(new
+            {
+                mensagem = "Esse ítem foi removido do registro",
+                produto
+            });
         }
     }
 }
